8e6675b32fe0ed6cd566f722245abc38ae86fe9a
xiaolin <xxxpo@139.com>
2024-05-23 01:48:06 +0800

    feat: 优化代码提示，表达式内标签也会作为提示

--- src/main/kotlin/com/xxxlin/jsgf/JsgfCompletionContributor.kt
+++ src/main/kotlin/com/xxxlin/jsgf/JsgfCompletionContributor.kt
@@ -5,31 +5,28 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
-import com.xxxlin.jsgf.psi.JsgfRule
 import com.xxxlin.jsgf.psi.JsgfTypes
+
 
 /**
  * 代码提示
  */
 class JsgfCompletionContributor : CompletionContributor() {
     init {
-        extend(
-            CompletionType.BASIC, PlatformPatterns.psiElement(JsgfTypes.SLOP_STRING_LITERAL),
+        extend(CompletionType.BASIC,
+            PlatformPatterns.psiElement(JsgfTypes.SLOP_STRING_LITERAL),
             object : CompletionProvider<CompletionParameters>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters,
-                    context: ProcessingContext,
-                    result: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet
                 ) {
+                    print(parameters.originalFile)
                     val properties = find(parameters.originalFile)
                     properties.forEach {
-                        val text = it.replace("<", "")
-                            .replace(">", "")
+                        val text = it.replace("<", "").replace(">", "")
                         result.addElement(LookupElementBuilder.create(text))
                     }
                 }
-            }
-        )
+            })
     }
 
     fun find(e: PsiElement): List<String> {
@@ -39,14 +36,9 @@ class JsgfCompletionContributor : CompletionContributor() {
     }
 
     private fun find(e: PsiElement, result: MutableList<String>) {
-        if (e is JsgfRule) {
-            val key = e.key
-            if (key != null) {
-                result.add(key)
-            }
-        }
-        if (e == JsgfTypes.RULE_NAME) {
-            result.add(e.node.text)
+        val ruleName = e.node.findChildByType(JsgfTypes.RULE_NAME)
+        if (ruleName != null) {
+            result.add(ruleName.text)
         }
         e.children.forEach {
             find(it, result)
